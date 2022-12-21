e95fa9c34be5f5e55709d16ed195e1850da7201d
adarshTS <adarsh.ts8@gmail.com>
2022-12-22 00:39:55 +0530

    modified function syntax

--- tests/firstTest.spec.js
+++ tests/firstTest.spec.js
@@ -16,7 +16,7 @@ const caps = {
 };
 
 
-test('test1', async () => {
+test('bstackDemo', async () => {
 
   const browser = await chromium.connect({
     wsEndpoint: `wss://cdp.browserstack.com/playwright?caps=${encodeURIComponent(JSON.stringify(caps))}`});
