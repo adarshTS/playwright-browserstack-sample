8026719199ad1a33c6f93e7a22d3764aef56870d
adarshTS <adarsh.ts8@gmail.com>
2022-12-22 14:15:26 +0530

    running on browserstack, locator issue unresolved

--- tests/firstTest.spec.js
+++ tests/firstTest.spec.js
@@ -5,7 +5,7 @@ const cp = require('child_process');
 const clientPlaywrightVersion = cp.execSync('npx playwright --version').toString().trim().split(' ')[1];  
 
 const caps = {
-  'browser': 'chrome',
+  'browser': 'playwright-firefox',
   'os': 'os x',
   'os_version': 'big sur',
   'name': 'Playwright',
@@ -16,33 +16,40 @@ const caps = {
 };
 
 
-test('bstackDemo', async ({ page }) => {
+
+test('bstackDemo', async () => {
 
   const browser = await chromium.connect({
-    wsEndpoint: `wss://cdp.browserstack.com/playwright?caps=${encodeURIComponent(JSON.stringify(caps))}`});
-
+    wsEndpoint: `wss://cdp.browserstack.com/playwright?caps=${encodeURIComponent(JSON.stringify(caps))}`
+  });
+ 
+  const page = await browser.newPage();
+  
   await page.goto("https://bstackdemo.com/");
-  await page.getByRole("link", { name: "Sign In" }).click();
-  await page.locator("#username svg").click();
-  await page.locator("#react-select-2-option-0-0").click();
-  await page.locator("#password svg").click();
-  await page.locator("#react-select-3-option-0-0").click();
-  await page.getByRole("button", { name: "Log In" }).click();
-  await page.locator('[id="\\31 "]').getByText("Add to cart").click();
-  await page.getByText("Samsung").click();
-  await page.locator('[id="\\31 5"]').getByText("Add to cart").click();
-  await page.getByText("Checkout").click();
-  await page.getByLabel("First Name").click();
-  await page.getByLabel("First Name").fill("Adarsh");
-  await page.getByLabel("Last Name").click();
-  await page.getByLabel("Last Name").fill("T S");
-  await page.getByLabel("Address").click();
-  await page.getByLabel("Address").fill("ABC");
-  await page.getByLabel("State/Province").click();
-  await page.getByLabel("State/Province").fill("KL");
-  await page.getByLabel("Postal Code").click();
-  await page.getByLabel("Postal Code").fill("555555");
-  await page.getByRole("button", { name: "Submit" }).click();
-  await expect(page.locator(".form-legend")).toHaveText("Your Order has been successfully placed.");
+  // await page.getByRole("link", { name: "Sign In" }).click;
+
+  // locator.click: Unknown engine "internal:text" while parsing selector internal:text="Select Username"i
+
+  // await page.getByText('Select Username').click();
+  // await page.locator("#react-select-2-option-0-0").click();
+  // await page.locator("#password svg").click();
+  // await page.locator("#react-select-3-option-0-0").click();
+  // await page.getByRole("button", { name: "Log In" }).click();
+  // await page.locator('[id="\\31 "]').getByText("Add to cart").click();
+  // await page.getByText("Samsung").click();
+  // await page.locator('[id="\\31 5"]').getByText("Add to cart").click();
+  // await page.getByText("Checkout").click();
+  // await page.getByLabel("First Name").click();
+  // await page.getByLabel("First Name").fill("Adarsh");
+  // await page.getByLabel("Last Name").click();
+  // await page.getByLabel("Last Name").fill("T S");
+  // await page.getByLabel("Address").click();
+  // await page.getByLabel("Address").fill("ABC");
+  // await page.getByLabel("State/Province").click();
+  // await page.getByLabel("State/Province").fill("KL");
+  // await page.getByLabel("Postal Code").click();
+  // await page.getByLabel("Postal Code").fill("555555");
+  // await page.getByRole("button", { name: "Submit" }).click();
+  // await expect(page.locator(".form-legend")).toHaveText("Your Order has been successfully placed.");
   await browser.close();
 });
